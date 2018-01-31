6a81921ce54540bb02a5bd2fe4e5cf329a4ba64f
Tech-A22 <tech-a22@Tech-A22s-MacBook-Air.local>
2018-01-31 01:15:50 +0000

    README complete

--- app.js
+++ app.js
@@ -9,56 +9,64 @@ do {
   input = prompt('Would you like to play Rock, Paper, Scissors?\n(y)Yes\n(n)No');
   if (input=='n') {
     playAgain = false;
+  } else if (input=='y') {
+    while (turns<=5 && usrScore <3 && cpuScore<3) {
+      var cpuRand = Math.floor(Math.random()*3);
+      var usrChoice = prompt('(r)Rock\n(p)Paper\n(s)Scissors');
+      switch (cpuChoice[cpuRand]) {
+        case 'Rock':
+          if (usrChoice =='r') {
+            alert('CPU picked: '+cpuChoice[cpuRand]+'. It\'s a draw');
+          } else if (usrChoice == 'p') {
+            alert('CPU picked: '+cpuChoice[cpuRand]+'. You are the WinRaR');
+            usrScore++;
+          } else if (usrChoice == 's') {
+            alert('CPU picked: '+cpuChoice[cpuRand]+'. You lost');
+            cpuScore++;
+          } else {
+            alert('CPU wins by default, pick a real choice next time');
+            cpuScore++;
+          }
+          break;
+        case 'Paper':
+          if (usrChoice =='r') {
+            alert('CPU picked: '+cpuChoice[cpuRand]+'. You lost');
+            cpuScore++;
+          } else if (usrChoice == 'p') {
+            alert('CPU picked: '+cpuChoice[cpuRand]+'. It\'s a draw');
+          } else if (usrChoice == 's') {
+            alert('CPU picked: '+cpuChoice[cpuRand]+'. You are the WinRaR');
+            usrScore++;
+          } else {
+            alert('CPU wins by default, pick a real choice next time');
+            cpuScore++;
+          }
+          break;
+        case 'Scissors':
+          if (usrChoice =='r') {
+            alert('CPU picked: '+cpuChoice[cpuRand]+'. You are the WinRaR');
+            usrScore++;
+          } else if (usrChoice == 'p') {
+            alert('CPU picked: '+cpuChoice[cpuRand]+'. You lost');
+            cpuScore++;
+          } else if (usrChoice == 's') {
+            alert('CPU picked: '+cpuChoice[cpuRand]+'. It\'s a draw');
+          } else {
+            alert('CPU wins by default, pick a real choice next time');
+            cpuScore++;
+          }
+          break;
+        default:
+      }//switch close
+      turns++;
+      alert('CPU score: '+cpuScore+'\nYour Score: '+usrScore);
+    }//while loop close
   }
-  while (turns>=5&&usrScore<=3&&cpuScore<=3) {
-    var cpuRand = Math.floor(Math.random()*3);
-    var usrChoice = prompt('(r)Rock\n(p)Paper\n(s)Scissors');
-    switch (cpuChoice[cpuRand]) {
-      case 'Rock':
-        if (usrChoice =='r') {
-          alert('CPU picked :'+cpuChoice[cpuRand]+'. It\'s a draw');
-        } else if (usrChoice == 'p') {
-          alert('CPU picked :'+cpuChoice[cpuRand]+'. You are the WinRAaR');
-          usrScore++;
-        } else if (usrChoice == 's') {
-          alert('CPU picked :'+cpuChoice[cpuRand]+'. You lost');
-          cpuScore++;
-        } else {
-          alert('CPU wins by default, pick a real choice next time');
-          cpuScore++;
-        }
-        break;
-      case 'Paper':
-        if (usrChoice =='r') {
-          alert('CPU picked :'+cpuChoice[cpuRand]+'. You lost');
-          cpuScore++;
-        } else if (usrChoice == 'p') {
-          alert('CPU picked :'+cpuChoice[cpuRand]+'. It\'s a draw');
-        } else if (usrChoice == 's') {
-          alert('CPU picked :'+cpuChoice[cpuRand]+'. You are the WinRAaR');
-          usrScore++;
-        } else {
-          alert('CPU wins by default, pick a real choice next time');
-          cpuScore++;
-        }
-        break;
-      case 'Scissors':
-        if (usrChoice =='r') {
-          alert('CPU picked :'+cpuChoice[cpuRand]+'. You are the WinRAaR');
-          usrScore++;
-        } else if (usrChoice == 'p') {
-          alert('CPU picked :'+cpuChoice[cpuRand]+'. You lost');
-          cpuScore++;
-        } else if (usrChoice == 's') {
-          alert('CPU picked :'+cpuChoice[cpuRand]+'. It\'s a draw');
-        } else {
-          alert('CPU wins by default, pick a real choice next time');
-          cpuScore++;
-        }
-        break;
-      default:
-    }
-    turn++;
-    alert('CPU score: '+cpuScore+'\nYour Score: '+usrScore);
+  if(usrScore>cpuScore){
+    alert('Winner, winner chicken dinner!');
+  } else if (cpuScore>usrScore) {
+    alert('You take the L, embaressment fills your heart, how will you ever recover?');
+  } else if(usrScore==cpuScore){
+    alert('A draw, how very normal.');
   }
 } while (playAgain);
